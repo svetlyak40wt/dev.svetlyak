a357b0411dbd4566fc19e32071e19654e0ae9b18
Alexander Artemenko <svetlyak.40wt@gmail.com>
2011-06-15 14:56:11 +0400

    Ограничение длинны фида до 20 статей.

--- ext/__init__.py
+++ ext/__init__.py
@@ -1,6 +1,13 @@
 import pelican
 
+DEFAULT_MAX_FEED_LEN = 20
+
 class Writer(pelican.writers.Writer):
+    def write_feed(self, elements, *args, **kwargs):
+        # Limit all atom feeds
+        max_feed_len = self.settings.get('MAX_FEED_LEN', DEFAULT_MAX_FEED_LEN)
+        return super(Writer, self).write_feed(elements[:max_feed_len], *args, **kwargs)
+
     def _add_item_to_the_feed(self, feed, item):
         feed.add_item(
             title = item.title,
@@ -12,7 +19,8 @@ class Writer(pelican.writers.Writer):
             pubdate = item.date,
         )
 
+
 class Pelican(pelican.Pelican):
     def get_writer(self):
-        return Writer(self.output_path)
+        return Writer(self.output_path, settings=self.settings)
 
